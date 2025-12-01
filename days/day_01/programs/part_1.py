1e9166e7ba719bc0e5c804869c352727c83bf637
AndreaBruno97 <andbruno97@gmail.com>
2025-12-01 19:16:22 +0100

    Day 1

--- days/day_01/programs/part_1.py
+++ days/day_01/programs/part_1.py
@@ -1,5 +1,10 @@
 from common import *
 
+DIAL_SIZE = 100
+DIAL_START = 50
+LEFT = "L"
+RIGHT = "R"
+
 
 class Part_1(BaseClass):
 
@@ -7,11 +12,25 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        input_lines = open_file_lines(filepath)
 
-        return -1
+        cur_pos = DIAL_START
+        zero_count = 0
+
+        for input_line in input_lines:
+            direction = input_line[0]
+            distance = int(input_line[1:])
+
+            if direction == LEFT:
+                distance *= -1
+
+            cur_pos = (cur_pos + distance) % DIAL_SIZE
+            if cur_pos == 0:
+                zero_count += 1
+
+        return zero_count
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(3)
 p1.execute()
--- days/day_01/programs/part_2.py
+++ days/day_01/programs/part_2.py
@@ -1,4 +1,10 @@
 from common import *
+import math
+
+DIAL_SIZE = 100
+DIAL_START = 50
+LEFT = "L"
+RIGHT = "R"
 
 
 class Part_2(BaseClass):
@@ -7,11 +13,26 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        input_lines = open_file_lines(filepath)
 
-        return -1
+        cur_pos = DIAL_START
+        zero_count = 0
+
+        for input_line in input_lines:
+            direction = input_line[0]
+            distance = int(input_line[1:])
+
+            for i in range(distance):
+                delta = 1 if direction == RIGHT else -1
+
+                cur_pos = (cur_pos + delta) % DIAL_SIZE
+
+                if cur_pos == 0:
+                    zero_count += 1
+
+        return zero_count
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(6)
 p2.execute()
