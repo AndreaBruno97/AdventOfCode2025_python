bbbf261abe773612afc32efabb7f852425f1c786
AndreaBruno97 <andbruno97@gmail.com>
2025-12-02 18:27:27 +0100

    Day 2

--- days/day_02/programs/part_1.py
+++ days/day_02/programs/part_1.py
@@ -7,11 +7,24 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        range_line_list = open_file(filepath).split(",")
 
-        return -1
+        invalid_id_total = 0
+
+        for range_line in range_line_list:
+            range_min, range_max = [int(x) for x in range_line.split("-")]
+
+            for cur_id in range(range_min, range_max + 1):
+                cur_id_str = str(cur_id)
+                cur_id_len = len(cur_id_str)
+                cur_half_point = int(cur_id_len/2)
+
+                if cur_id_str[0:cur_half_point] == cur_id_str[cur_half_point:]:
+                    invalid_id_total += cur_id
+
+        return invalid_id_total
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(1227775554)
 p1.execute()
--- days/day_02/programs/part_2.py
+++ days/day_02/programs/part_2.py
@@ -7,11 +7,26 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        range_line_list = open_file(filepath).split(",")
 
-        return -1
+        invalid_id_total = 0
+
+        for range_line in range_line_list:
+            range_min, range_max = [int(x) for x in range_line.split("-")]
+
+            for cur_id in range(range_min, range_max + 1):
+                cur_id_str = str(cur_id)
+                cur_id_len = len(cur_id_str)
+
+                for block_len in range(1, int(cur_id_len/2)+1):
+                    repeat_times = int(cur_id_len/block_len)
+                    if cur_id_str[0:block_len] * repeat_times == cur_id_str:
+                        invalid_id_total += cur_id
+                        break
+
+        return invalid_id_total
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(4174379265)
 p2.execute()
