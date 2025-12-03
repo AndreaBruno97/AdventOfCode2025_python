171d089f9066b0804b7be7d1b109b14d757d4d77
AndreaBruno97 <andbruno97@gmail.com>
2025-12-03 23:06:46 +0100

    Day 3

--- days/day_03/programs/part_1.py
+++ days/day_03/programs/part_1.py
@@ -7,11 +7,32 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        file_line_list = open_file_lines(filepath)
 
-        return -1
+        total_joltage = 0
+
+        for line in file_line_list:
+            bank = [int(x) for x in line]
+            bank_size = len(bank)
+            first_max = 0
+            first_pos = -1
+            second_max = 0
+
+            for cur_battery in range(bank_size-1):
+                if bank[cur_battery] > first_max:
+                    first_max = bank[cur_battery]
+                    first_pos = cur_battery
+
+            for cur_battery in range(first_pos+1, bank_size):
+                if bank[cur_battery] > second_max:
+                    second_max = bank[cur_battery]
+
+            cur_joltage = (10 * first_max) + second_max
+            total_joltage += cur_joltage
+
+        return total_joltage
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(357)
 p1.execute()
--- days/day_03/programs/part_2.py
+++ days/day_03/programs/part_2.py
@@ -1,5 +1,7 @@
 from common import *
 
+BATTERY_NUM = 12
+
 
 class Part_2(BaseClass):
 
@@ -7,11 +9,32 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        file_line_list = open_file_lines(filepath)
 
-        return -1
+        total_joltage = 0
+
+        for line in file_line_list:
+            bank = [int(x) for x in line]
+            bank_size = len(bank)
+            start_battery = -1
+            cur_joltage = 0
+
+            for cur_iteration in range(BATTERY_NUM, 0, -1):  # from BATTERY_NUM to 1
+                end_battery = bank_size - cur_iteration + 1
+                cur_max_battery = 0
+
+                for cur_battery in range(start_battery+1, end_battery):
+                    if bank[cur_battery] > cur_max_battery:
+                        cur_max_battery = bank[cur_battery]
+                        start_battery = cur_battery
+
+                cur_joltage = (10 * cur_joltage) + cur_max_battery
+
+            total_joltage += cur_joltage
+
+        return total_joltage
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(3121910778619)
 p2.execute()
