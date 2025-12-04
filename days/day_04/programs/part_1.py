b8f21ef8a8aab11d7e59bc55799162a9c79a5a05
AndreaBruno97 <andbruno97@gmail.com>
2025-12-04 20:57:27 +0100

    Day 4

--- days/day_04/programs/part_1.py
+++ days/day_04/programs/part_1.py
@@ -1,4 +1,9 @@
 from common import *
+import numpy as np
+
+PAPER = "@"
+EMPTY = "."
+MAX_NEAR = 3
 
 
 class Part_1(BaseClass):
@@ -7,11 +12,26 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        grid = open_file_str_matrix_guarded(filepath, EMPTY)
+        cols = len(grid)
+        rows = len(grid[0])
+        total_paper = 0
 
-        return -1
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if grid[r][c] == EMPTY:
+                    continue
+
+                submatrix = grid[r-1:r+2, c-1:c+2]
+                total_near = np.count_nonzero(submatrix == PAPER)
+                total_near -= 1  # Not counting the paper itself in the middle
+
+                if total_near <= MAX_NEAR:
+                    total_paper += 1
+
+        return total_paper
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(13)
 p1.execute()
--- days/day_04/programs/part_2.py
+++ days/day_04/programs/part_2.py
@@ -1,4 +1,9 @@
 from common import *
+import numpy as np
+
+PAPER = "@"
+EMPTY = "."
+MAX_NEAR = 3
 
 
 class Part_2(BaseClass):
@@ -7,11 +12,33 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        grid = open_file_str_matrix_guarded(filepath, EMPTY)
+        cols = len(grid)
+        rows = len(grid[0])
+        total_paper = 0
 
-        return -1
+        is_update = True
+
+        while is_update:
+            is_update = False
+
+            for r in range(1, rows):
+                for c in range(1, cols):
+                    if grid[r][c] == EMPTY:
+                        continue
+
+                    submatrix = grid[r - 1:r + 2, c - 1:c + 2]
+                    total_near = np.count_nonzero(submatrix == PAPER)
+                    total_near -= 1  # Not counting the paper itself in the middle
+
+                    if total_near <= MAX_NEAR:
+                        is_update = True
+                        total_paper += 1
+                        grid[r][c] = EMPTY
+
+        return total_paper
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(43)
 p2.execute()
