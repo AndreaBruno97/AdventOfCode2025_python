28aaa085e10e8421ef138050b44ebc4e0c8b940b
AndreaBruno97 <andbruno97@gmail.com>
2025-12-06 15:28:10 +0100

    Day 6

--- days/day_06/programs/part_1.py
+++ days/day_06/programs/part_1.py
@@ -1,4 +1,9 @@
 from common import *
+import re
+import numpy as np
+
+PLUS = "+"
+TIMES = "*"
 
 
 class Part_1(BaseClass):
@@ -7,11 +12,29 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        file_lines = open_file_lines(filepath)
+        problem_matrix = [
+            [y for y in re.split(r'\s+', x.strip())]
+            for x in file_lines
+        ]
+        rows = len(problem_matrix)
+        cols = len(problem_matrix[0])
 
-        return -1
+        problem_matrix = np.concatenate(problem_matrix).reshape(rows, cols).T
+
+        grand_total = 0
+        for cur_problem in problem_matrix:
+            cur_operation = cur_problem[-1]
+            cur_operand_list = [int(x) for x in cur_problem[:-1]]
+
+            if cur_operation == PLUS:
+                grand_total += sum(cur_operand_list)
+            else:
+                grand_total += np.prod(cur_operand_list, dtype=object)
+
+        return grand_total
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(4277556)
 p1.execute()
--- days/day_06/programs/part_2.py
+++ days/day_06/programs/part_2.py
@@ -1,4 +1,9 @@
 from common import *
+import re
+import numpy as np
+
+PLUS = "+"
+TIMES = "*"
 
 
 class Part_2(BaseClass):
@@ -7,11 +12,31 @@ class Part_2(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        file_matrix = open_file_str_matrix(filepath)
+        rows = len(file_matrix)
+        cols = len(file_matrix[0])
 
-        return -1
+        problem_matrix = np.concatenate(file_matrix).reshape(rows, cols).T
+
+        empty_rows_list = [index for index, x in enumerate(problem_matrix == ' ') if x.all()]
+        problem_list = [x if index == 0 else x[1:, :] for index, x in
+                        enumerate(np.split(problem_matrix, empty_rows_list))]
+
+        grand_total = 0
+
+        for cur_problem in problem_list:
+            cur_operation = cur_problem[0, -1]
+            cur_operand_matrix = cur_problem[:, :-1]
+            cur_operand_list = [int(''.join(x)) for x in cur_operand_matrix]
+
+            if cur_operation == PLUS:
+                grand_total += sum(cur_operand_list)
+            else:
+                grand_total += np.prod(cur_operand_list, dtype=object)
+
+        return grand_total
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(3263827)
 p2.execute()
