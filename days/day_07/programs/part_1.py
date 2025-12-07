8248f6810e0aa7a6ebd4fe8eb81437ce6756c1d0
AndreaBruno97 <andbruno97@gmail.com>
2025-12-07 15:01:10 +0100

    Day 7

--- days/day_07/programs/part_1.py
+++ days/day_07/programs/part_1.py
@@ -1,17 +1,49 @@
 from common import *
 
 
+START = "S"
+EMPTY = "."
+SPLIT = "^"
+
+
 class Part_1(BaseClass):
 
     def __init__(self):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        manifold = open_file_str_matrix(filepath)
+        rows = len(manifold)
+        cols = len(manifold[0])
 
-        return -1
+        # Set of column positions for each beam
+        beam_set = set()
+        # Set of row/column coordinates for each split
+        split_set = set()
+
+        for r in range(rows):
+            for c in range(cols):
+                if manifold[r][c] == START:
+                    beam_set.add(c)
+                elif manifold[r][c] == SPLIT:
+                    split_set.add((r, c))
+
+        split_count = 0
+
+        for r in range(rows):
+            new_beam_set = set()
+            for beam in beam_set:
+                if (r, beam) in split_set:
+                    split_count += 1
+                    new_beam_set.add(beam-1)
+                    new_beam_set.add(beam+1)
+                else:
+                    new_beam_set.add(beam)
+            beam_set = new_beam_set
+
+        return split_count
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(21)
 p1.execute()
--- days/day_07/programs/part_2.py
+++ days/day_07/programs/part_2.py
@@ -1,17 +1,50 @@
 from common import *
 
 
+START = "S"
+EMPTY = "."
+SPLIT = "^"
+
+
 class Part_2(BaseClass):
 
     def __init__(self):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        manifold = open_file_str_matrix(filepath)
+        rows = len(manifold)
+        cols = len(manifold[0])
 
-        return -1
+        # Dictionary of column positions for each beam (value is the number of occurrences)
+        beam_dict = {}
+        # Set of row/column coordinates for each split
+        split_set = set()
+
+        for r in range(rows):
+            for c in range(cols):
+                if manifold[r][c] == START:
+                    beam_dict[c] = 1
+                elif manifold[r][c] == SPLIT:
+                    split_set.add((r, c))
+
+        for r in range(rows):
+            new_beam_dict = {}
+            for beam in beam_dict.keys():
+
+                if (r, beam) in split_set:
+                    new_left = new_beam_dict.get(beam-1) or 0
+                    new_right = new_beam_dict.get(beam+1) or 0
+                    new_beam_dict[beam-1] = new_left + beam_dict[beam]
+                    new_beam_dict[beam+1] = new_right + beam_dict[beam]
+                else:
+                    new_num = new_beam_dict.get(beam) or 0
+                    new_beam_dict[beam] = new_num + beam_dict[beam]
+            beam_dict = new_beam_dict
+
+        return sum(beam_dict.values())
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(40)
 p2.execute()
