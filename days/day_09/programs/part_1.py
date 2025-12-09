da98598d74cf466e5813667a86d1f5c92283c8e2
AndreaBruno97 <andbruno97@gmail.com>
2025-12-09 23:21:19 +0100

    Day 9 - part 1

--- days/day_09/programs/part_1.py
+++ days/day_09/programs/part_1.py
@@ -7,11 +7,23 @@ class Part_1(BaseClass):
         super().__init__()
 
     def execute_internal(self, filepath):
-        print(open_file(filepath))
+        tile_list = [[int(x) for x in tile_str.split(",")] for tile_str in open_file_lines(filepath)]
+        tile_num = len(tile_list)
 
-        return -1
+        max_area = 0
+
+        for index_1 in range(tile_num):
+            for index_2 in range(index_1+1, tile_num):
+                x_1, y_1 = tile_list[index_1]
+                x_2, y_2 = tile_list[index_2]
+
+                cur_area = abs(x_1 - x_2 + 1) * abs(y_1 - y_2 + 1)
+                if cur_area > max_area:
+                    max_area = cur_area
+
+        return max_area
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(50)
 p1.execute()
